e470230ed6542fead3fa02acb16e280f634f1bf3
allyssoncomin <allysson_luan@hotmail.com>
2023-10-23 19:41:11 -0300

    att

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # All tasks
+  get 'tasks', to: 'tasks#index'
+
+  # Show task
+  get 'tasks/:id', to: 'tasks#show'
+  
 end
